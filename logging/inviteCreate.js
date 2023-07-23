1d191cdfb3ceb2aef0a6e6baa043b14c1175133a
Robin Painter <lochlanpainter@gmail.com>
2023-07-23 13:29:06 +1000

    Update inviteCreate.js

--- logging/inviteCreate.js
+++ logging/inviteCreate.js
@@ -17,13 +17,22 @@ module.exports = {
 		} else {
 			inviteEnd = Math.floor(currentDate+invite.maxAge)
 		}
+		var inviter
+		
+		if(invite.inviter.displayName) {
+		    inviter = invite.inviter.displayName
+		} else if(invite.inviter.username) {
+		    inviter = invite.inviter.username
+		} else {
+		    inviter = ""
+		}
 
 		const embed = new EmbedBuilder()
 			.setColor(embedColor)
 			if(invite.maxAge === 0) {
-				embed.setDescription("An infinite invite was created by "+invite.inviter.username+", and has "+invite.maxUses+" max uses\nhttps://discord.gg/"+invite.code);
+				embed.setDescription("An infinite invite was created by "+inviter+", and has "+invite.maxUses+" max uses\nhttps://discord.gg/"+invite.code);
 			} else {
-				embed.setDescription("An invite was created by "+invite.inviter.username+", it will end <t:"+inviteEnd+":R> and has "+invite.maxUses+" max uses\nhttps://discord.gg/"+invite.code);
+				embed.setDescription("An invite was created by "+inviter+", it will end <t:"+inviteEnd+":R> and has "+invite.maxUses+" max uses\nhttps://discord.gg/"+invite.code);
 			}
 			embed.setTimestamp();
 		client.channels.cache.get(logsID).send({ embeds: [embed] });

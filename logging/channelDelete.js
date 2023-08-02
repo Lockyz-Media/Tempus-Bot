51e03c7b6222d5a6b0b6353cfd83283e118173ae
Robin P <lochlanpainter@gmail.com>
2023-08-02 12:08:27 +1000

    Update channelDelete.js

--- logging/channelDelete.js
+++ logging/channelDelete.js
@@ -1,5 +1,5 @@
 const { EmbedBuilder } = require('discord.js');
-const { embedColor, ownerID } = require('../config');
+const { embedcolours, ownerID } = require('../config');
 
 module.exports = {
 	name: 'channelDelete',
@@ -62,7 +62,7 @@ module.exports = {
 		}
 
 		const embed = new EmbedBuilder()
-			.setColor(embedColor)
+			.setColor(embedcolours.negative)
 			if(isThread) {
 				embed.setDescription("A thread named "+channel.name)
 			} else {

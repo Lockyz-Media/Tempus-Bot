2fe586a3b33cb2c959103eeb5d96df39dfffd4e0
Robin P <lochlanpainter@gmail.com>
2023-08-02 11:46:46 +1000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, Message, ButtonStyle } = require('discord.js')
-const { commandMetrics } = require('../functions.js')
+const { commandMetrics } = require('../features/miscFunctions.js')
 const SQLite = require("better-sqlite3");
 const sql = new SQLite('./bot.sqlite');
 

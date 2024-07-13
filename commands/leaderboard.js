c1d11b88bdc91f642386f0a658aa722a310f7327
Robin P <lochlanpainter@gmail.com>
2024-07-13 16:25:11 +1000

    Removed unneccessary leaderboard options (only top25 is "required")

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -18,31 +18,10 @@ module.exports = {
 			pl: 'Rasa psa',
 			de: 'Hunderasse',
 		})*/
-		.setDMPermission(false)
-		.addStringOption((option) =>
-            option.setName('leaderboard')
-			/*.setNameLocalizations({
-				pl: 'pies',
-				de: 'hund',
-			})*/
-            .setDescription('Which leaderboard did you wanna use?')
-			/*.setDescriptionLocalizations({
-				pl: 'Rasa psa',
-				de: 'Hunderasse',
-			})*/
-            .setRequired(true)
-            .addChoices(
-                { name: 'Top 5', value: 'top5'},
-                { name: 'Top 10', value: 'top10'},
-                { name: 'Top 15', value: 'top15'},
-                { name: 'Top 20', value: 'top20'},
-				{ name: 'Top 25', value: 'top25'},
-            )
-        ),
+		.setDMPermission(false),
 	async execute(interaction) {
 		commandMetrics(interaction.client, "leaderboard", interaction.guild.id, interaction.user.id)
         const client = interaction.client
-		const leaderboard = interaction.options.getString('leaderboard')
 
         const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
 	    if (!table['count(*)']) {
@@ -64,10 +43,6 @@ module.exports = {
                 score = { id: `${interaction.guild.id}-${interaction.user.id}`, user: interaction.user.id, guild: interaction.guild.id, points: 0, level: 0 };
             }
 
-            const top5 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 5;").all(interaction.guild.id);
-			const top10 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 10;").all(interaction.guild.id);
-			const top15 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 15;").all(interaction.guild.id);
-			const top20 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 20;").all(interaction.guild.id);
 			const top25 = sql.prepare("SELECT * FROM scores WHERE guild = ? ORDER BY points DESC LIMIT 25;").all(interaction.guild.id);
 			let i = 1;
 
@@ -76,200 +51,44 @@ module.exports = {
 				.setTitle("Leaderboard")
 				.setDescription("Discord.js changed the way embeds are made, we have not worked out how to make this command work with that feature just yet.\nIf you would like to view your points use /userinfo instead for now.")
 
-			if(leaderboard === 'top5') {
-				embed.setTitle("Top Five")
-				embed.setDescription("Our top 5 points leaders!")
+			
+			embed.setTitle("Top Twenty-Five")
+			embed.setDescription("Our top 25 points leaders!")
 
-				for(const data of top5) {
-					let user = interaction.guild.members.cache.get(data.user)
-					function nFormatter(num, digits) {
-						const lookup = [
-							{ value: 1, symbol: "" },
-							{ value: 1e3, symbol: "k" },
-							{ value: 1e6, symbol: "M" },
-							{ value: 1e9, symbol: "G" },
-							{ value: 1e12, symbol: "T" },
-							{ value: 1e15, symbol: "P" },
-							{ value: 1e18, symbol: "E" }
-						];
+			for(const data of top25) {
+				let user = interaction.guild.members.cache.get(data.user)
+				function nFormatter(num, digits) {
+					const lookup = [
+						{ value: 1, symbol: "" },
+						{ value: 1e3, symbol: "k" },
+						{ value: 1e6, symbol: "M" },
+						{ value: 1e9, symbol: "G" },
+						{ value: 1e12, symbol: "T" },
+						{ value: 1e15, symbol: "P" },
+						{ value: 1e18, symbol: "E" }
+					];
 	
-						const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-						  var item = lookup.slice().reverse().find(function(item) {
-							return num >= item.value;
-						  });
-						  return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
-					}
+					const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+						var item = lookup.slice().reverse().find(function(item) {
+						return num >= item.value;
+						});
+						return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
+				}
+	
+				if(i <= 6) {
 					if(!user) {
-						embed.addFields({ name: i.toString()+')', type: '**<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**'})
+						embed.addField('\u200b', i.toString()+') **<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**');
 					} else {
-						embed.addFields({ name: i.toString()+')', type: '**<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**'})
+						embed.addField('\u200b', i.toString()+') **<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**');
 					}
-					i++
-				  }
-			}
-
-			if(leaderboard === 'top10') {
-				embed.setTitle("Top Ten")
-				embed.setDescription("Our top 10 points leaders!")
-
-				for(const data of top10) {
-					let user = interaction.guild.members.cache.get(data.user)
-					function nFormatter(num, digits) {
-						const lookup = [
-							{ value: 1, symbol: "" },
-							{ value: 1e3, symbol: "k" },
-							{ value: 1e6, symbol: "M" },
-							{ value: 1e9, symbol: "G" },
-							{ value: 1e12, symbol: "T" },
-							{ value: 1e15, symbol: "P" },
-							{ value: 1e18, symbol: "E" }
-						];
-	
-						const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-						  var item = lookup.slice().reverse().find(function(item) {
-							return num >= item.value;
-						  });
-						  return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
-					}
-	
-					if(i <= 6) {
-						if(!user) {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**'});
-						} else {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**'});
-						}
+				} else {
+					if(!user) {
+						embed.addField('\u200b', i.toString()+') **<@'+data.user+'>** at **Level '+nFormatter(data.level, 0)+'**');
 					} else {
-						if(!user) {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** at **Level '+nFormatter(data.level, 0)+'**'});
-						} else {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** at **Level '+nFormatter(data.level, 0)+'**'});
-						}
+						embed.addField('\u200b', i.toString()+') **<@'+data.user+'>** at **Level '+nFormatter(data.level, 0)+'**');
 					}
-					i++
-				  }
-			}
-
-			if(leaderboard === 'top15') {
-				embed.setTitle("Top Fifteen")
-				embed.setDescription("Our top 15 points leaders!")
-
-				for(const data of top15) {
-					let user = interaction.guild.members.cache.get(data.user)
-					function nFormatter(num, digits) {
-						const lookup = [
-							{ value: 1, symbol: "" },
-							{ value: 1e3, symbol: "k" },
-							{ value: 1e6, symbol: "M" },
-							{ value: 1e9, symbol: "G" },
-							{ value: 1e12, symbol: "T" },
-							{ value: 1e15, symbol: "P" },
-							{ value: 1e18, symbol: "E" }
-						];
-	
-						const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-						  var item = lookup.slice().reverse().find(function(item) {
-							return num >= item.value;
-						  });
-						  return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
-					}
-	
-					if(i <= 6) {
-						if(!user) {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**'});
-						} else {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**'});
-						}
-					} else {
-						if(!user) {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** at **Level '+nFormatter(data.level, 0)+'**'});
-						} else {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** at **Level '+nFormatter(data.level, 0)+'**'});
-						}
-					}
-					i++
-				  }
-			}
-
-			if(leaderboard === 'top20') {
-				embed.setTitle("Top Twenty")
-				embed.setDescription("Our top 20 points leaders!")
-
-				for(const data of top20) {
-					let user = interaction.guild.members.cache.get(data.user)
-					function nFormatter(num, digits) {
-						const lookup = [
-							{ value: 1, symbol: "" },
-							{ value: 1e3, symbol: "k" },
-							{ value: 1e6, symbol: "M" },
-							{ value: 1e9, symbol: "G" },
-							{ value: 1e12, symbol: "T" },
-							{ value: 1e15, symbol: "P" },
-							{ value: 1e18, symbol: "E" }
-						];
-	
-						const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-						  var item = lookup.slice().reverse().find(function(item) {
-							return num >= item.value;
-						  });
-						  return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
-					}
-	
-					if(i <= 6) {
-						if(!user) {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**'});
-						} else {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**'});
-						}
-					} else {
-						if(!user) {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** at **Level '+nFormatter(data.level, 0)+'**'});
-						} else {
-							embed.addFields({ name: '\u200b', value: i.toString()+') **<@'+data.user+'>** at **Level '+nFormatter(data.level, 0)+'**'});
-						}
-					}
-					i++
-				  }
-			}
-
-			if(leaderboard === 'top25') {
-				embed.setTitle("Top Twenty-Five")
-				embed.setDescription("Our top 25 points leaders!")
-
-				for(const data of top25) {
-					let user = interaction.guild.members.cache.get(data.user)
-					function nFormatter(num, digits) {
-						const lookup = [
-							{ value: 1, symbol: "" },
-							{ value: 1e3, symbol: "k" },
-							{ value: 1e6, symbol: "M" },
-							{ value: 1e9, symbol: "G" },
-							{ value: 1e12, symbol: "T" },
-							{ value: 1e15, symbol: "P" },
-							{ value: 1e18, symbol: "E" }
-						];
-	
-						const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-						  var item = lookup.slice().reverse().find(function(item) {
-							return num >= item.value;
-						  });
-						  return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
-					}
-	
-					if(i <= 6) {
-						if(!user) {
-							embed.addField('\u200b', i.toString()+') **<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**');
-						} else {
-							embed.addField('\u200b', i.toString()+') **<@'+data.user+'>** with over **'+nFormatter(data.points, 2)+' XP** and at **Level '+nFormatter(data.level, 0)+'**');
-						}
-					} else {
-						if(!user) {
-							embed.addField('\u200b', i.toString()+') **<@'+data.user+'>** at **Level '+nFormatter(data.level, 0)+'**');
-						} else {
-							embed.addField('\u200b', i.toString()+') **<@'+data.user+'>** at **Level '+nFormatter(data.level, 0)+'**');
-						}
-					}
-					i++
-				  }
+				}
+				i++
 			}
 
 	  		return interaction.reply({ embeds: [embed] });
